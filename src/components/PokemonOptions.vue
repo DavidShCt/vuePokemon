75c4f5306e7f4315c2c340fcba4abd7cd048822c
David <davidj_gacevedo@quimica.unam.mx>
2023-03-10 10:59:18 -0600

    pokemonOptions with vuetify

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,13 +1,44 @@
 <template>
-  <h3>PokemonOptions</h3>
+  <v-container>
+    <v-list two-line>
+      <v-list-item-group v-model="selectedPokemon">
+        <template v-for="(pokemon, index) in pokemons">
+          <v-list-item :key="pokemon.id" @click="$emit( 'selectionPokemon', pokemon.id )">>
+            <template v-slot:default>
+              <v-list-item-content>
+                <v-list-item-title>{{ pokemon.name }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+          <v-divider
+            v-if="index < pokemons.length - 1"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  name: 'PokemonOptions',
+  props:{
+    pokemons:{
+      type: Array,
+      required: true
+    }
+  },
+  methods:{
+    
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
 
+}
 </style>
