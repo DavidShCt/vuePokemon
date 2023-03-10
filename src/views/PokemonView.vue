018f8d6958096791faaa4d022fabbf57dfe90d60
David <davidj_gacevedo@quimica.unam.mx>
2023-03-10 11:28:41 -0600

    pokemon view

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,30 +1,71 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-col cols="12">
-        <div>¿Quíen es ese pokémon?</div>
-      </v-col>
-      <v-col cols="12">
-        <pokemon-picture/>
-      </v-col>
-      <v-col cols="12">
-        <pokemon-options/>
-      </v-col>
-    </v-row>
-  </v-container>
+    <div v-if="!pokemon">Espere por favor...</div>
 
+    <div v-else>
+      <h1>¿Quíen es ese pokémon?</h1>
+
+      <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+
+      <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+
+      <template v-if="showAnswer">
+        <h2 class="fade-in">{{ message }}</h2>
+        <v-btn @click="newGame">Nuevo Juego</v-btn>
+      </template>
+    </div>
+  </v-container>
 </template>
 
 <script>
-import PokemonOptions from '../components/PokemonOptions.vue'
-import PokemonPicture from '../components/PokemonPicture.vue'
+import PokemonOptions from "../components/PokemonOptions.vue";
+import PokemonPicture from "../components/PokemonPicture.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 export default {
-  name: 'PokemonView',
-  components:{PokemonOptions, PokemonPicture}
-}
+  name: "PokemonView",
+  components: { PokemonOptions, PokemonPicture },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: "",
+    };
+  },
+  methods: {
+    async mixPokemonsArray() {
+      this.pokemonArr = await getPokemonOptions();
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+
+    checkAnswer(selectedId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto, es ${this.pokemon.name}`;
+      } else {
+        this.message = `ups, era ${this.pokemon.name}`;
+      }
+    },
+
+    newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.mixPokemonArray();
+    },
+  },
+  mounted(){
+    this.mixPokemonsArray()
+  }
+};
 </script>
 <style scoped>
-.container{
+.container {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
