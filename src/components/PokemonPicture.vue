65579098f2ae3bf4517132d5d4d20ae265059a6d
David <davidj_gacevedo@quimica.unam.mx>
2023-03-10 10:38:14 -0600

    components pkemon and styles

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      v-if="showPokemon"
+      src="https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      alt="pokemon"
+      class="hidden-pokemon"
+    />
+    <img 
+      v-if="!showPokemon" 
+      src="https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      alt="pokemon"
+      class="fade-in"
+    />
+  </div>
+</template>
+s
+<script>
+export default {
+  name: "PokemonPicture",
+  data(){
+    return{
+      showPokemon: false
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.showPokemon = true
+    }, 1000);
+  }
+}
+</script>
+
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+</style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>¿Quíen es este pokémon?</div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <div>¿Quíen es ese pokémon?</div>
+      </v-col>
+      <v-col cols="12">
+        <pokemon-picture/>
+      </v-col>
+      <v-col cols="12">
+        <pokemon-options/>
+      </v-col>
+    </v-row>
+  </v-container>
 
 </template>
 
 <script>
+import PokemonOptions from '../components/PokemonOptions.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
 export default {
-  name: 'PokemonView'
+  name: 'PokemonView',
+  components:{PokemonOptions, PokemonPicture}
 }
 </script>
+<style scoped>
+.container{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
