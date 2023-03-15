8bb42fdcc65cb28e4a5bec86e7c31cc634a5847d
David <davidj_gacevedo@quimica.unam.mx>
2023-03-15 09:20:25 -0600

    modificaciones app y pluggin vuetify

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
       dark
     >
       <v-toolbar-title>PokeApp</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text @click="$vuetify.theme.dark = !$vuetify.theme.dark">Dark Mode</v-btn>
     </v-app-bar>
     <PokemonView/>
   </div>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,27 +1,8 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vuetify from 'vuetify/lib';
 import es from 'vuetify/lib/locale/es';
 
 Vue.use(Vuetify);
-
-export const vuetify = new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#00589c',
-        secondary: '#004179',
-        accent: '#c5911e',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-        'gris-10': '#5e5e5f',
-        'text-gray': '#666666',
-        'text-primary': '#0c71c3'
-      }
-    },
-  },
-})
 
 export default new Vuetify({
     lang: {
